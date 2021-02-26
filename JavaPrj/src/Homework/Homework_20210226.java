27e2cbed837280476b144978e33b929a0dd261f7
형준 <79628837+stem47950@users.noreply.github.com>
2021-02-26 13:28:04 +0900

    small modifications

--- JavaPrj/src/Homework/Homework_20210226.java
+++ JavaPrj/src/Homework/Homework_20210226.java
@@ -17,12 +17,12 @@ public class Homework_20210226 {
         System.out.println("│       성적 출력       │");
         System.out.println("└───────────────────────┘");
 
-        System.out.printf("국어: %6.2f\n", (float) kor);
-        System.out.printf("영어: %6.2f\n", (float) eng);
-        System.out.printf("수학: %6.2f\n", (float) math);
-        System.out.println();
+        System.out.printf("  국어: %6.2f\n", (float) kor);
+        System.out.printf("  영어: %6.2f\n", (float) eng);
+        System.out.printf("  수학: %6.2f\n", (float) math);
+        System.out.println(" ──────────────");
 
-        System.out.printf("총점: %6.2f\n", (float) total);
-        System.out.printf("평균: %6.2f\n", avg);
+        System.out.printf("  총점: %6.2f\n", (float) total);
+        System.out.printf("  평균: %6.2f\n", avg);
     }
 }
