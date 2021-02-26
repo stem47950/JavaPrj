936ca66cfd65e83bfe6a337bc2dc57c965eaddca
형준 <79628837+stem47950@users.noreply.github.com>
2021-02-26 13:15:36 +0900

    reformat

--- JavaPrj/src/Hello.java
+++ JavaPrj/src/Hello.java
@@ -1,5 +1,5 @@
-public class Hello{
-    public static void main(String[] args){
+public class Hello {
+    public static void main(String[] args) {
         int kor, eng, math;
         int total;
         float avg;
@@ -9,9 +9,9 @@ public class Hello{
         math = 80;
 
         total = kor + eng + math;
-        avg = (float)total / 3;
+        avg = (float) total / 3;
 
-        //System.out.printf("total is %d\n", total);
+        // System.out.printf("total is %d\n", total);
 
         System.out.println("┌───────────────────────┐");
         System.out.println("│       성적 출력       │");
@@ -20,7 +20,7 @@ public class Hello{
         System.out.printf("국어: %d\n", kor);
         System.out.printf("영어: %d\n", eng);
         System.out.printf("수학: %d\n", math);
-        
+
         System.out.printf("총점: %d\n", total);
         System.out.printf("평균: %5.2f\n", avg);
     }
