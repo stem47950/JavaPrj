3ca514885b00dc45b484f4393766d9fa5d74f585
형준 <79628837+stem47950@users.noreply.github.com>
2021-02-26 13:16:19 +0900

    add avg

--- JavaPrj/src/Homework/Homework_20210226.java
+++ JavaPrj/src/Homework/Homework_20210226.java
@@ -1,24 +1,28 @@
 package Homework;
 
 public class Homework_20210226 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int kor, eng, math;
         int total;
+        float avg;
 
         kor = 65;
         eng = 72;
         math = 86;
 
         total = kor + eng + math;
+        avg = (float) total / 3;
 
         System.out.println("┌───────────────────────┐");
         System.out.println("│       성적 출력       │");
         System.out.println("└───────────────────────┘");
 
-        System.out.printf("국어: %6.2f\n", (float)kor);
-        System.out.printf("영어: %6.2f\n", (float)eng);
-        System.out.printf("수학: %6.2f\n", (float)math);
+        System.out.printf("국어: %6.2f\n", (float) kor);
+        System.out.printf("영어: %6.2f\n", (float) eng);
+        System.out.printf("수학: %6.2f\n", (float) math);
+        System.out.println();
 
-        System.out.printf("총점: %6.2f\n", (float)total);
+        System.out.printf("총점: %6.2f\n", (float) total);
+        System.out.printf("평균: %6.2f\n", avg);
     }
 }
