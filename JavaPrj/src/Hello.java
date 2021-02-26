53c43c40944aad05162dee0c18ffe6a99fafa93c
형준 <79628837+stem47950@users.noreply.github.com>
2021-02-26 10:27:05 +0900

    using 'System.out.print()'

--- JavaPrj/src/Hello.java
+++ JavaPrj/src/Hello.java
@@ -11,6 +11,15 @@ public class Hello{
         total = kor + eng + math;
         avg = total / 3;
 
-        System.out.printf("total is %d\n", total);
+        //System.out.printf("total is %d\n", total);
+
+        System.out.print("┌───────────────────────┐\n");
+        System.out.print("│       성적 출력       │\n");
+        System.out.print("└───────────────────────┘\n");
+        System.out.print("국어: 0\n");
+        System.out.print("영어: 0\n");
+        System.out.print("수학: 0\n");
+        System.out.print("총점: 0\n");
+        System.out.print("평균: 0.00\n");
     }
 }
