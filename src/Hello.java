05f6efa148605a5058164d19ddaa8af1bf0ddf83
박형준 <79628837+stem47950@users.noreply.github.com>
2021-03-02 08:34:04 +0000

    added file io

--- src/Hello.java
+++ src/Hello.java
@@ -1,28 +1,26 @@
-import java.util.Scanner;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Hello {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // 기본형식: primitive type
         int kor, eng, math;
         int total;
         float avg;
-
-        System.out.println("┌───────────────────────┐");
-        System.out.println("│       성적 입력       │");
-        System.out.println("└───────────────────────┘");
-
-        kor = 0;
-        eng = 0;
-        math = 0;
         /*
+         * System.out.println("┌───────────────────────┐");
+         * System.out.println("│       성적 입력       │");
+         * System.out.println("└───────────────────────┘");
+         * 
          * Scanner scan = new Scanner(System.in);
          * 
          * System.out.print("국어 성적을 입력하세요.: "); kor = scan.nextInt();
          * System.out.print("영어 성적을 입력하세요.: "); eng = scan.nextInt();
          * System.out.print("수학 성적을 입력하세요.: "); math = scan.nextInt();
          */
-        FileInputStream fis = new FileInputStream("JavaPrj/data.txt");
+        FileInputStream fis = new FileInputStream("data.txt");
         Scanner scan1 = new Scanner(fis);
 
         kor = scan1.nextInt();
