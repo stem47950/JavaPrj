6110585f27f60e933538c25ba31a9c09d68cedd8
박형준 <79628837+stem47950@users.noreply.github.com>
2021-03-03 03:43:55 +0000

    new Program.java

---
+++ src/ex1/bit/Program.java
@@ -0,0 +1,10 @@
+package ex1.bit;
+
+import java.io.FileInputStream;
+
+public class Program {
+    public static void main(String[] args) {
+        // 1. 이미지 파일을 읽기 위한 파일입력 스트림을 fis로 생성하시오.
+        FileInputStream fis = new FileInputStream("img.bmp");
+    }
+}
