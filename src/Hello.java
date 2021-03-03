1dc000be0a4516cd8fe3135b4f097bc9b0047a54
박형준 <79628837+stem47950@users.noreply.github.com>
2021-03-03 02:16:41 +0000

    FileOutputStream(), PrintStream()

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,7 @@
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Hello {
@@ -8,7 +10,10 @@ public class Hello {
         int kor, eng, math;
         int total;
         float avg;
+
         /*
+         * 성적을 입력하는 코드
+         * 
          * System.out.println("┌───────────────────────┐");
          * System.out.println("│       성적 입력       │");
          * System.out.println("└───────────────────────┘");
@@ -19,12 +24,29 @@ public class Hello {
          * System.out.print("영어 성적을 입력하세요.: "); eng = scan.nextInt();
          * System.out.print("수학 성적을 입력하세요.: "); math = scan.nextInt();
          */
-        FileInputStream fis = new FileInputStream("data.txt");
-        Scanner scan1 = new Scanner(fis);
 
-        kor = scan1.nextInt();
-        eng = scan1.nextInt();
-        math = scan1.nextInt();
+        /*
+         * 파일 data.txt에서 국어, 영어, 수학 성적을 입력받으시오.
+         */
+        FileInputStream fis = new FileInputStream("data.txt"); // 파일 입력 스트림 객체를 fis라는 이름으로 생성
+        Scanner fin = new Scanner(fis); // 객체 fis를 입력으로 받는 Scanner 객체 생성
+        kor = fin.nextInt();
+        eng = fin.nextInt();
+        math = fin.nextInt();
+        fin.close(); // fin을 닫아줌.
+        fis.close(); // fis를 닫아줌.
+
+        /*
+         * 국어, 영어, 수학 성적을 파일 data.txt로 받아내시오.
+         */
+        FileOutputStream fos = new FileOutputStream("data.txt");
+        PrintStream fout = new PrintStream(fos);
+        kor++; // kor = kor + 1;
+        eng++; // eng = eng + 1;
+
+        fout.printf("%d %d %d", kor, eng, math);
+        fout.close();
+        fos.close();
 
         System.out.println("┌───────────────────────┐");
         System.out.println("│       성적 출력       │");
