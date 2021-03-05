1289fecc3abdb610ac470a74d62136d3da07d586
박형준 <79628837+stem47950@users.noreply.github.com>
2021-03-05 03:10:44 +0000

    짝수 조건과 같이 홀수 조건 추가

--- src/ex2/control/IfProgram.java
+++ src/ex2/control/IfProgram.java
@@ -8,6 +8,8 @@ public class IfProgram {
         int x = input.nextInt();
         if (x % 2 == 0)
             System.out.println("짝수 값");
+        if (x % 2 != 0)
+            System.out.println("홀수 값");
 
         System.out.println("프로그램 끝!");
     }
