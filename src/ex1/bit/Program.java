017465ae264acca86c68c75cead1805ed89fe3bc
박형준 <79628837+stem47950@users.noreply.github.com>
2021-03-05 03:03:24 +0000

    img.bmp 파일의 경로를 수정

--- src/ex1/bit/Program.java
+++ src/ex1/bit/Program.java
@@ -5,6 +5,6 @@ import java.io.FileInputStream;
 public class Program {
     public static void main(String[] args) {
         // 1. 이미지 파일을 읽기 위한 파일입력 스트림을 fis로 생성하시오.
-        FileInputStream fis = new FileInputStream("img.bmp");
+        FileInputStream fis = new FileInputStream("res/img.bmp");
     }
 }
