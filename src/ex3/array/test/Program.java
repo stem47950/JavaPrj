51f1e353c3049f212a589e61b890001869ecb01d
박형준 <79628837+stem47950@users.noreply.github.com>
2021-03-10 01:14:54 +0000

    배열 문제 풀이 1

---
+++ src/ex3/array/test/Program.java
@@ -0,0 +1,27 @@
+package ex3.array.test;
+
+public class Program {
+    public static void main(String[] args) {
+        // 1. 크기 10의 정수배열 nums를 생성한다.
+        int i, size;
+        int[] nums;
+
+        i = 0;
+        size = 10;
+        nums = new int[size];
+
+        // 2. 배열 nums에 정수 2, 4, 6, 8, ... 처럼 짝수 10개를 입력하는 반복문을 작성한다.
+        for (i = 0; i < size; i++)
+            nums[i] = 2 + 2 * i;
+
+        // 3. 배열 nums를 출력하는 코드를 작성한다.
+        // 출력하는 모양은 다음과 같다.
+        // 2, 4, 6, 8, [...], 20
+        for (i = 0; i < size; i++) {
+            if (i != 0)
+                System.out.print(", ");
+            System.out.print(nums[i]);
+        }
+        System.out.println();
+    }
+}
