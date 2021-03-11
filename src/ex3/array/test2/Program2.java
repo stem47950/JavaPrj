8e0edcda1c0ca759b531f343b4f73f939e62e6fa
박형준 <79628837+stem47950@users.noreply.github.com>
2021-03-11 02:55:48 +0000

    문자열 배열 속 원자들의 정렬

--- src/ex3/array/test2/Program2.java
+++ src/ex3/array/test2/Program2.java
@@ -6,24 +6,35 @@ public class Program2 {
     public static void main(String[] args) {
         String[] names;
         Random rand;
-        int i;
+        int i, j;
         String temp;
 
         names = new String[] { "강호동", "유재석", "하하", "김지호", "김현준", "김태희", "박민정" };
-        // String n = new String("hello");
-        // String n1 = "hello";
+        String n = new String("hello");
+        String n1 = "hello";
 
-        rand = new Random();
+        // rand = new Random();
 
-        for (i = 0; i < 100; i++) {
-            int idx1 = rand.nextInt(7);
-            int idx2 = rand.nextInt(7);
-            // System.out.printf("idx1: %d, idx2: %d\n", idx1, idx2);
+        // for (i = 0; i < 100; i++) {
+        // int idx1 = rand.nextInt(7);
+        // int idx2 = rand.nextInt(7);
+        // // System.out.printf("idx1: %d, idx2: %d\n", idx1, idx2);
 
-            temp = names[idx1];
-            names[idx1] = names[idx2];
-            names[idx2] = temp;
+        // temp = names[idx1];
+        // names[idx1] = names[idx2];
+        // names[idx2] = temp;
+        // }
+
+        for (j = 0; j < names.length - 1; j++) {
+            for (i = 0; i < names.length - 1 - j; i++) {
+                if (names[i].compareTo(names[i + 1]) > 0) {
+                    temp = names[i];
+                    names[i] = names[i + 1];
+                    names[i + 1] = temp;
+                }
+            }
         }
+
         for (i = 0; i < 7; i++) {
             if (i != 0)
                 System.out.print(", ");
