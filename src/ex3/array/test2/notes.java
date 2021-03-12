4b2f0f2c36924bbecc8c356eb8e482b51121c9ab
박형준 <79628837+stem47950@users.noreply.github.com>
2021-03-12 03:41:57 +0000

    String.indexOf(), String.compareTo()

---
+++ src/ex3/array/test2/notes.java
@@ -0,0 +1,10 @@
+package ex3.array.test2;
+
+public class notes {
+    public static void main(String[] args) {
+        String fileName = "photo.jpg";
+        
+        //System.out.println(fileName.substring(0, fileName.indexOf(".")));
+        System.out.println(fileName.compareTo("test.jpg"));
+    }
+}
